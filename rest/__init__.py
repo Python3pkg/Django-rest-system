109374500ff1f620bc25cb0e6ca9fabf3b79d8ee
G4brym Massadas <g4bryrm98@hotmail.com>
2016-12-03 14:17:16 +0000

    Updates

---
+++ rest/__init__.py
@@ -0,0 +1,3 @@
+from .manager import *
+from .endpoints.model import *
+from .endpoints.service import *
--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
+from rest import *
 
-setup(name='Django-rest-system',
-    version='1.0',
+setup(name='django_rest_system',
+    version='1.1',
     description='Full rest system for Django, will help you save hundreds of lines',
     url='https://github.com/G4brym/Django-rest-system',
     author='Gabriel Massadas',
     license='GPL-3.0',
-    packages=['funniest'],
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
