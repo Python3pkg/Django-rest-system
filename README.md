8ccb736a13187d76463c03b7774a77c9ab7449fe
G4brym Massadas <g4bryrm98@hotmail.com>
2016-12-24 14:13:13 +0000

    Updates

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Django-rest-system
-Full rest system for Django, will help you save hundreds of lines
+Full rest system for Django that will help you save hundreds of lines
 
 # Documentation
 Everything is documented here [Docs](https://github.com/G4brym/Django-rest-system/wiki)
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-rest-system',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     license='GPL-3.0',
@@ -25,6 +25,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
